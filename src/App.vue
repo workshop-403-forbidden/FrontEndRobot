4ce7843c6b83137a32988a2becc5bcb3fa9183ea
MAKSIM-BRY <maxim.barry@gmail.com>
2020-11-23 16:22:51 +0100

    Mse en place de l'écran d'acceuil.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <Home />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home,
   }
 }
 </script>
